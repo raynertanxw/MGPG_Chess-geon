5d5e63cffcd6f74ab1082f41714dd90b25f6ea27
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-05-02 12:54:57 +0800

    Created temp tile assets and basic grid

---
+++ Chess-geon/Assets/DungeonGrid/Scripts/DungeonBlock.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BlockState { Wall, Enemy, Empty, Player, Selectable };
+
+public class DungeonBlock
+{
+	private BlockState mState;
+	public BlockState State { get { return mState; } }
+
+	public DungeonBlock(BlockState _state)
+	{
+		mState = _state;
+	}
+}
---
+++ Chess-geon/Assets/DungeonGrid/Scripts/DungeonManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonManager : MonoBehaviour
+{
+	public int sizeX, sizeY;
+	public int scaleMultiplier = 1;
+	private float blockSize;
+	private float halfBlockSize;
+	public GameObject dungeonBlockPrefab;
+	public Sprite blackTileSprite, whiteTileSprite, selectableTileSprite, wallTileSprite;
+
+	private DungeonBlock[,] dungeonBlockGrid = null;
+	private GameObject[,] dungeonBlockGameObjectGrid = null;
+	private SpriteRenderer[,] dungeonBlockSpriteRens = null;
+
+	void Awake()
+	{
+		blockSize = blackTileSprite.rect.xMax / 100.0f * scaleMultiplier;
+		halfBlockSize = blockSize / 2.0f;
+
+		CreateDungeonBlocks();
+		CreateDungeonGameObjects();
+	}
+
+	private void CreateDungeonBlocks()
+	{
+		if (dungeonBlockGrid != null)
+		{
+			Debug.LogWarning("dungeonBlockGrid is already created.");
+			return;
+		}
+
+		dungeonBlockGrid = new DungeonBlock[sizeX, sizeY];
+		for (int y = 0; y < sizeY; y++)
+		{
+			for (int x = 0; x < sizeX; x++)
+			{
+				DungeonBlock curBlock;
+
+				// Edge Cases
+				if (x == 0 || x == sizeX - 1 || y == 0 || y == sizeY - 1)
+				{
+					curBlock = new DungeonBlock(BlockState.Wall);
+					dungeonBlockGrid[x, y] = curBlock;
+					continue;
+				}
+
+				curBlock = new DungeonBlock(BlockState.Empty);
+				dungeonBlockGrid[x, y] = curBlock;
+			}
+		}
+	}
+
+	private bool IsWhiteTile(int x, int y)
+	{
+		// White is even-even, odd-odd. Black is even-odd, odd-even.
+		if (x % 2 == y % 2)
+			return true;
+		else
+			return false;
+	}
+
+	private void CreateDungeonGameObjects()
+	{
+		if (dungeonBlockGameObjectGrid != null || dungeonBlockSpriteRens != null)
+		{
+			Debug.LogWarning("DungeonBlockGameObjectGrid or dungeonBlockSpriteRens is already created.");
+			return;
+		}
+
+		dungeonBlockGameObjectGrid	= new GameObject[sizeX, sizeY];
+		dungeonBlockSpriteRens		= new SpriteRenderer[sizeX, sizeY];
+		for (int y = 0; y < sizeY; y++)
+		{
+			for (int x = 0; x < sizeX; x++)
+			{
+				Vector3 curBlockPos = transform.position;
+				curBlockPos.x += halfBlockSize + (x * blockSize);
+				curBlockPos.y += halfBlockSize + (y * blockSize);
+				GameObject curBlock = (GameObject) Instantiate(dungeonBlockPrefab, curBlockPos, Quaternion.identity);
+				curBlock.transform.localScale = Vector3.one * scaleMultiplier;
+				dungeonBlockGameObjectGrid[x, y] = curBlock;
+
+				dungeonBlockSpriteRens[x, y] = curBlock.GetComponent<SpriteRenderer>();
+				switch (dungeonBlockGrid[x, y].State)
+				{
+				case BlockState.Wall:
+					dungeonBlockSpriteRens[x, y].sprite = wallTileSprite;
+					break;
+				case BlockState.Selectable:
+					dungeonBlockSpriteRens[x, y].sprite = selectableTileSprite;
+					break;
+				default:
+					if (IsWhiteTile(x, y))	// White Tile
+						dungeonBlockSpriteRens[x, y].sprite = whiteTileSprite;
+					else	// Black Tile
+						dungeonBlockSpriteRens[x, y].sprite = blackTileSprite;
+					break;
+				}
+			}
+		}
+	}
+}
