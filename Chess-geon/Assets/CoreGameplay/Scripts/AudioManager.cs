7b9020e8f94bb747a3e7b7192cc8804268e078f6
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-07-01 01:31:11 +0800

    Implemented AudioManager
    - Only for BGM so far.

---
+++ Chess-geon/Assets/CoreGameplay/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+	private static AudioManager sInstance = null;
+	public static AudioManager Instance { get { return sInstance; } }
+
+	void Awake()
+	{
+		if (sInstance == null)
+		{
+			sInstance = this;
+			Setup();
+		}
+		else if (sInstance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+	}
+
+
+
+
+
+	private void Setup()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
