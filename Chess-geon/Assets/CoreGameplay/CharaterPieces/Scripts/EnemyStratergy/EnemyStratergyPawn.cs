b2a1faee8b509220bfa1ebeafdaeb2e3f6d42103
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-06-30 10:36:34 +0800

    Pawn EnemyStrategy fully implemented
    - Pawn now attacks
    - Pawn now randomly moves when on same level as player.

--- Chess-geon/Assets/CoreGameplay/CharaterPieces/Scripts/EnemyStratergy/EnemyStratergyPawn.cs
+++ Chess-geon/Assets/CoreGameplay/CharaterPieces/Scripts/EnemyStratergy/EnemyStratergyPawn.cs
@@ -9,6 +9,49 @@ public class EnemyStratergyPawn : EnemyStratergy
 		GridManager grid = DungeonManager.Instance.Grids[(int)GridType.Pawn];
 		LinkedList<Node> retPath = null;
 
+		// Attack
+		if ((_enemyPosY - GameManager.Instance.Player.PosY) == 1)	// Player below
+		{
+			// Check if Player is below right.
+			if (DungeonManager.Instance.IsPlayerPos(_enemyPosX + 1, _enemyPosY - 1))
+			{
+				retPath = new LinkedList<Node>();
+				retPath.AddFirst(grid.nodes[_enemyPosX + 1, _enemyPosY - 1]);
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY]);
+				return retPath;
+			}
+
+			// Check if Player is below left.
+			if (DungeonManager.Instance.IsPlayerPos(_enemyPosX - 1, _enemyPosY - 1))
+			{
+				retPath = new LinkedList<Node>();
+				retPath.AddFirst(grid.nodes[_enemyPosX - 1, _enemyPosY - 1]);
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY]);
+				return retPath;
+			}
+		}
+		else if ((_enemyPosY - GameManager.Instance.Player.PosY) == -1)	// Player above
+		{
+			// Check if Player is above right.
+			if (DungeonManager.Instance.IsPlayerPos(_enemyPosX + 1, _enemyPosY + 1))
+			{
+				retPath = new LinkedList<Node>();
+				retPath.AddFirst(grid.nodes[_enemyPosX + 1, _enemyPosY + 1]);
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY]);
+				return retPath;
+			}
+
+			// Check if Player is above left.
+			if (DungeonManager.Instance.IsPlayerPos(_enemyPosX - 1, _enemyPosY + 1))
+			{
+				retPath = new LinkedList<Node>();
+				retPath.AddFirst(grid.nodes[_enemyPosX - 1, _enemyPosY + 1]);
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY]);
+				return retPath;
+			}
+		}
+
+		// Movement.
 		if (_enemyPosY > GameManager.Instance.Player.PosY)
 		{
 			if (DungeonManager.Instance.IsCellEmpty(_enemyPosX, _enemyPosY - 1))
@@ -29,7 +72,28 @@ public class EnemyStratergyPawn : EnemyStratergy
 		}
 		else	// Pawn is in same Y pos as player.
 		{
-			// Do Nothing.
+			// Random up or down..
+			int randDirection = Random.Range(0, 2);
+			if (randDirection == 0)
+				randDirection = 1;
+			else
+				randDirection = -1;
+
+			// Check if valid move initially.
+			if (DungeonManager.Instance.IsCellEmpty(_enemyPosX, _enemyPosY + randDirection))
+			{
+				retPath = new LinkedList<Node>();
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY + randDirection]);
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY]);
+			}
+			// Check if other only posisble move left is possible.
+			else if (DungeonManager.Instance.IsCellEmpty(_enemyPosX, _enemyPosY - randDirection))
+			{
+				retPath = new LinkedList<Node>();
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY - randDirection]);
+				retPath.AddFirst(grid.nodes[_enemyPosX, _enemyPosY]);
+			}
+			// Otherwise it's just a poor little stuck pawn.
 		}
 
 		return retPath;
