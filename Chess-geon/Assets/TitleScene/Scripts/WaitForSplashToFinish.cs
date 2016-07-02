e0fcaaca6b58dbb9e4b1f9a60284fa3c9f7cb93f
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-07-02 21:24:27 +0800

    Added WaitForSpalsh scene
    - Deals mostly with problem on iOS where splash screen not done yet but
    scene already playing.

---
+++ Chess-geon/Assets/TitleScene/Scripts/WaitForSplashToFinish.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WaitForSplashToFinish : MonoBehaviour
+{	
+	void Update()
+	{
+		if (Application.isShowingSplashScreen == false)
+		{
+			SceneManager.LoadScene("TitleScene");
+		}
+	}
+}
